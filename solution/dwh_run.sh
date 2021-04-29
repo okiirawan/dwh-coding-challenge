738ea4cb6e282045ad184f6da273a201ecc96ffd
Oki Ayodya Irawan <oki.irawan@transdigital.co.id>
2021-04-29 22:37:35 +0700

    move directory

--- solution/dwh_run.sh
+++ solution/dwh_run.sh
@@ -3,6 +3,5 @@
 echo "=========Build Docker=============="
 docker build -t dwh:latest .
 
-cd ..
 echo "=========Run docker And  get result=========="
 docker run --rm -v $PWD/data:/data -v $PWD/result:/result dwh:latest
